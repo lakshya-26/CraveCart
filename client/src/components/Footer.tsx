c5706b7db0a9b024591f76946c1bd8fb694b6a77
lakshya-26 <lakshyakumarsingh.2003.va@gmail.com>
2024-07-07 01:43:46 +0530

    landing page done

---
+++ client/src/components/Footer.tsx
@@ -0,0 +1,21 @@
+
+const Footer = () => {
+  return (
+    <div className="bg-orange-500 py-10">
+        <div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
+            <span className="text-3xl text-white font-bold tracking-tight">
+                CraveCart
+            </span>
+            <span className="text-white font-bold tracking-tight flex gap-4">
+                <span>Privacy Policy</span>
+                <span>Terms of service</span>
+            </span>
+            <span className="text-white font-bold tracking-tight">
+                &copy;copyright-2024
+            </span>
+        </div>
+    </div>
+  )
+}
+
+export default Footer
--- client/src/layouts/layout.tsx
+++ client/src/layouts/layout.tsx
@@ -1,4 +1,6 @@
+import Footer from "@/components/Footer";
 import Header from "@/components/Header";
+import Hero from "@/components/Hero";
 
 
 type Props = {
@@ -10,8 +12,9 @@ const layout = ({children}: Props) => {
   return (
     <div className="flex flex-col min-h-screen">
         <Header />
-
+        <Hero />
         <div className="container mx-auto flex-1 py-10">{children}</div>
+        <Footer />
     </div>
   )
 }
