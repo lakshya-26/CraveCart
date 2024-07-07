03cb0be8393d7e275cb837f4edfbd6c2c0cde445
lakshya-26 <lakshyakumarsingh.2003.va@gmail.com>
2024-07-08 01:13:25 +0530

    auth0 access token validation complete

--- client/src/auth/Auth0ProviderWithNavigate.tsx
+++ client/src/auth/Auth0ProviderWithNavigate.tsx
@@ -9,8 +9,9 @@ const Auth0ProviderWithNavigate = ({ children }: Props) => {
   const domain = import.meta.env.VITE_AUTH0_DOMAIN;
   const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
   const redirectUri = import.meta.env.VITE_AUTH0_CALLBACK_URL;
+  const audience = import.meta.env.VITE_AUTH0_AUDIENCE;
 
-  if (!domain || !clientId || !redirectUri) {
+  if (!domain || !clientId || !redirectUri || !audience) {
     throw new Error("Missing Auth0 configuration");
   }
 
@@ -26,6 +27,7 @@ const Auth0ProviderWithNavigate = ({ children }: Props) => {
       clientId={clientId}
       authorizationParams={{
         redirect_uri: redirectUri,
+        audience: audience,
       }}
       onRedirectCallback={onRedirectCallback}
     >
--- server/src/routes/userRoutes.ts
+++ server/src/routes/userRoutes.ts
@@ -1,9 +1,10 @@
 import express from "express";
 import myUserController from "../controllers/user.controller";
+import { jwtCheck } from "../middlewares/auth.middlewares";
 
 const router = express.Router();
 
-router.post("/", myUserController.createCurrentUser);
+router.post("/", jwtCheck, myUserController.createCurrentUser);
 
 
 export default router;
