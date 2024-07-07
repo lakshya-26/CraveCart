ea0c7e93c3f7283ea319536df2aa6bf69e33873e
lakshya-26 <lakshyakumarsingh.2003.va@gmail.com>
2024-07-08 00:52:37 +0530

    validated access token

--- client/src/auth/Auth0ProviderWithNavigate.tsx
+++ client/src/auth/Auth0ProviderWithNavigate.tsx
@@ -1,11 +1,11 @@
-import { useCreateMyUser } from "@/api/MyUserApi";
 import { AppState, Auth0Provider, User } from "@auth0/auth0-react";
+import { useNavigate } from "react-router-dom";
 
 type Props = {
   children: React.ReactNode;
 };
 const Auth0ProviderWithNavigate = ({ children }: Props) => {
-  const {createUser} = useCreateMyUser();
+  const navigate = useNavigate();
   const domain = import.meta.env.VITE_AUTH0_DOMAIN;
   const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
   const redirectUri = import.meta.env.VITE_AUTH0_CALLBACK_URL;
@@ -15,12 +15,10 @@ const Auth0ProviderWithNavigate = ({ children }: Props) => {
   }
 
   const onRedirectCallback = (appState?: AppState, user?: User) => {
-        console.log("USER", user);
-        console.log("APPSTATE", appState);
-        if(user?.sub && user?.email){
-          createUser({auth0Id: user.sub, email: user.email});
-        }
-  }
+    console.log("USER", user);
+    console.log("APPSTATE", appState);
+    navigate("/auth-callback");
+  };
 
   return (
     <Auth0Provider
