fed30ef46664f4e90ee756f047d37fdcd585615d
lakshya-26 <lakshyakumarsingh.2003.va@gmail.com>
2024-07-08 14:05:34 +0530

    user profile form

--- client/src/layouts/layout.tsx
+++ client/src/layouts/layout.tsx
@@ -5,14 +5,15 @@ import Hero from "@/components/Hero";
 
 type Props = {
     children: React.ReactNode;
+    showHero?: boolean;
 }
 
 
-const layout = ({children}: Props) => {
+const layout = ({children, showHero = false}: Props) => {
   return (
     <div className="flex flex-col min-h-screen">
         <Header />
-        <Hero />
+        {showHero && <Hero />}
         <div className="container mx-auto flex-1 py-10">{children}</div>
         <Footer />
     </div>
---
+++ client/src/pages/UserProfilePage.tsx
@@ -0,0 +1,9 @@
+import UserProfileForm from "@/forms/user-profle-form/UserProfileForm"
+
+const UserProfilePage = () => {
+  return (
+    <UserProfileForm />
+  )
+}
+
+export default UserProfilePage
