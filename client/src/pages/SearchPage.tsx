a518c8ae7acf77ff5964139cd686452aae382eb5
lakshya-26 <lakshyakumarsingh.2003.va@gmail.com>
2024-07-15 20:53:01 +0530

    search api integrated with frontend

--- client/src/pages/SearchPage.tsx
+++ client/src/pages/SearchPage.tsx
@@ -1,9 +1,20 @@
+import { useSearchRestaurants } from "@/api/RestaurantAPi";
 import { useParams } from "react-router-dom"
 
 const SearchPage = () => {
     const {city} = useParams();
+    const {results} = useSearchRestaurants(city);
+
+    
   return (
+    <>
     <div>User search for {city}</div>
+    {results?.data.map((restaurant) => (
+      <span>
+        found - {restaurant.restaurantName}, {restaurant.city}
+      </span>
+    ))}
+    </>
   )
 }
 
