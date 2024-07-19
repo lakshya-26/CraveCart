4bbdbf6ee110f76d70af2f08466792aa11e62270
lakshya-26 <lakshyakumarsingh.2003.va@gmail.com>
2024-07-19 21:40:40 +0530

    search filter done

--- client/src/api/RestaurantAPi.tsx
+++ client/src/api/RestaurantAPi.tsx
@@ -1,14 +1,17 @@
+import { SearchState } from "@/pages/SearchPage";
 import { RestaurantSearchResponse } from "@/types";
 import { useQuery } from "react-query";
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export const useSearchRestaurants = (
+    searchState: SearchState,
     city?: string
   ) => {
     const createSearchRequest = async (): Promise<RestaurantSearchResponse> => {
       const params = new URLSearchParams();
-  
+      params.set("searchQuery", searchState.searchQuery);
+
       const response = await fetch(
         `${API_BASE_URL}/api/restaurant/search/${city}?${params.toString()}`
       );
@@ -21,7 +24,7 @@ export const useSearchRestaurants = (
     };
 
     const { data: results, isLoading } = useQuery(
-        ["searchRestaurants"],
+        ["searchRestaurants", searchState],
         createSearchRequest,
         { enabled: !!city }
       );
--- client/src/pages/SearchPage.tsx
+++ client/src/pages/SearchPage.tsx
@@ -1,13 +1,34 @@
 import { useSearchRestaurants } from "@/api/RestaurantAPi";
+import SearchBar, { SearchForm } from "@/components/SearchBar";
 import SearchResultCard from "@/components/SearchResultCard";
 import SearchResultInfo from "@/components/SearchResultInfo";
+import { useState } from "react";
 import { useParams } from "react-router-dom";
 
-
+export type SearchState = {
+  searchQuery: string;
+};
 
 const SearchPage = () => {
   const { city } = useParams();
-  const { results, isLoading } = useSearchRestaurants(city);
+  const [searchState, setSearchState] = useState<SearchState>({
+    searchQuery: "",
+  });
+  const { results, isLoading } = useSearchRestaurants(searchState, city);
+
+  const setSearchQuery = (searchFormData: SearchForm) => {
+    setSearchState((prevState) => ({
+      ...prevState,
+      searchQuery: searchFormData.searchQuery,
+    }));
+  };
+
+  const resetSearch = () => {
+    setSearchState((prevState) => ({
+      ...prevState,
+      searchQuery: "",
+    }));
+  };
 
   if (isLoading) {
     <span>Loading...</span>;
@@ -20,12 +41,17 @@ const SearchPage = () => {
       <div className="grid grid-cols-1 lg:grid-cols-[250px_1fr] gap-5">
         <div id="cuisines-list">insert cusinies here :)</div>
         <div id="main-content" className="flex- flex-col gap-5">
+          <SearchBar
+            onSubmit={setSearchQuery}
+            placeHolder="Search by cuisine or restaurant name"
+            onReset={resetSearch}
+            searchQuery={searchState.searchQuery}
+          />
           <SearchResultInfo total={results.pagination.total} city={city} />
           {results.data.map((restaurant) => (
-            <SearchResultCard  restaurant={restaurant}/>
+            <SearchResultCard restaurant={restaurant} />
           ))}
         </div>
-        
       </div>
     </>
   );
